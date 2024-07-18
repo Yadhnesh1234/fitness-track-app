7adc9c32388085dcad0dfc13781287d5d5a46c59
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-07-18 13:12:05 +0530

    Table Component Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SaveuserworkoutComponent } from './components/saveuserworkout/saveuserworkout.component';
 import { authGuard } from './auth.guard';
-import { UserTableComponent } from './components/user-table/user-table.component';
+import { UserslistComponent } from './components/userslist/userslist.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
-    { path: 'getalluser',component:UserTableComponent,canActivate:[authGuard]},
     { path: 'saveworkout',component:SaveuserworkoutComponent,canActivate:[authGuard]},
+    { path: 'userlist',component:UserslistComponent,canActivate:[authGuard]},
     { path:'', redirectTo:"/login",pathMatch:"full"},
     { path:'**',  loadComponent: () => import('./components/notfound/notfound.component').then(m => m.NotfoundComponent)},
 ];
--- src/app/components/userslist/userslist.component.ts
+++ src/app/components/userslist/userslist.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DbService } from '../../services/db.service';
+import {DataTablesModule} from 'angular-datatables'
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-userslist',
   standalone: true,
-  imports: [],
+  imports: [DataTablesModule],
   templateUrl: './userslist.component.html',
   styleUrl: './userslist.component.css'
 })
-export class UserslistComponent {
-
+export class UserslistComponent implements OnInit {
+     usersList:any=[]
+     dtoptions:any=[]
+     dttrigger:any=new Subject<any>()
+     constructor(private dbSevice:DbService){
+     }
+     ngOnInit(): void {
+      this.loadUsers()
+      this.dtoptions={
+        pagingType:"full"
+      }
+     }
+     loadUsers(){
+        for(let i=1;i<100;i++){
+          this.usersList.push({
+            id:1,
+            username:"Raj",
+            workouttype:"cardio",
+            duration:2,
+            progress:"20%"
+          })
+        }
+     }
 }
