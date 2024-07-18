8f8f45177dc0777a414f6dfdd13f0ab8941ce7e8
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-07-18 17:57:52 +0530

    Bug Fixes UI changes added

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBpcgZfGoOdAKuZk6wFOtYFwg5tpXdcMG0",
+    authDomain: "fitness-track-55c91.firebaseapp.com",
+    projectId: "fitness-track-55c91",
+    storageBucket: "fitness-track-55c91.appspot.com",
+    messagingSenderId: "800427425893",
+    appId: "1:800427425893:web:e5e9b1e7f47bee9c23068c"
+  };
